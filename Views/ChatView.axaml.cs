aefceebb5e688da24825f15c789fae228cc358a3
Louis Landois <louis.landois@proton.me>
2024-03-29 16:31:38 +0100

    feat(Navigation): Switch between view from navbar
    Refactoring MainView so it embedded HomeView by default. User can now switch view by cliking on the desired one from navbar

---
+++ Views/ChatView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ynov_Workshare.Views;
+
+public partial class ChatView : UserControl
+{
+    public ChatView()
+    {
+        InitializeComponent();
+    }
+}
